734a0f14b8fe8da8a3e38afc0cb286a524f12dfd
lmp50 <lmp59.yandex.ru>
2020-01-24 21:22:00 +0300

    Составлен набор функций

--- qlogvirtualtable.cpp
+++ qlogvirtualtable.cpp
@@ -32,8 +32,28 @@ bool QLogVirtualTable::addFieldDefs(const bool &bKey, const QString &sFieldName,
     return true;
 }
 
-bool QLogVirtualTable::insert()
+bool QLogVirtualTable::insertEditPost(int iAction, int iParamNum, ...)
 {
+//#include <stdio.h>
+//#include <stdarg.h>
+
+    int result = 0;
+    va_list factor;         //указатель va_list
+    va_start(factor, iParamNum);    // устанавливаем указатель
+    for(int i=0; i<iParamNum; i++)
+    {
+        if (m_vFieldDefs[i].m_ftFieldType == ftInt)
+            result += va_arg(factor, int);  // получаем значение текущего параметра типа int
+        else if (m_vFieldDefs[i].m_ftFieldType == ftUInt)
+            result += va_arg(factor, unsigned int);  // получаем значение текущего параметра типа int
+        else if (m_vFieldDefs[i].m_ftFieldType == ftDouble)
+            result += va_arg(factor, double);  // получаем значение текущего параметра типа int
+    }
+    va_end(factor); // завершаем обработку параметров
+//    return result;
+
+
+
 
     return true;
 }
--- qlogvirtualtable.h
+++ qlogvirtualtable.h
@@ -5,7 +5,7 @@
 #include <QMap>
 #include <QVector>
 
-enum enumFieldDefs {ftInt,ftUInt,ftBool,ftString};
+enum enumFieldDefs {ftInt,ftUInt,ftDouble,ftBool,ftString};
 
 struct FieldDefs
 {
@@ -23,8 +23,48 @@ public:
     explicit QLogVirtualTable(QObject *parent = nullptr);
 
     bool addFieldDefs(const bool &bKey, const QString &sFieldName, enumFieldDefs ftFieldType, const int &iSize, const bool &bRequired);
-    bool insert();
+    bool insertEditPost(int iAction,int iParamNum, ...);// iAction == 0 то добавить запись если её нет и редактировать если запись с таким ключом есть
+                                                    // iAction == 1 только добавлять запись с таким ключом, если есть запись с таким ключом то ничего не делать
+                                                    // iAction == 2 только редактировать
 
+    bool insert();
+    bool edit();
+    bool post();
+    bool deleteRecord(int iParamNum,...); //Удаление записи по ключу
+    bool deleteRecord(int iRecordNum);    // Удаление записи по номеру
+    bool deleteRecord(); // Удалить текущую запись
+    void clear();        // Удалитьвсе записи
+
+    QString getFieldByName(QString sFiledName, bool *bOk = nullptr);
+    bool setFieldByName(QString sFiledName,QString sValue);
+
+    bool FieldByName(QString sFiledName,quint8 quint8Data);
+    bool FieldByName(QString sFiledName,quint16 quint16Data);
+
+    QString getField(int iFieldNum);
+    bool setField(int iFieldNum,QString sValue);
+
+    bool FindKey(int iKey,...);
+    bool Locate(QString sLocate);  // Строка по правилам упрощенного SQL
+    bool LocateF(QString sLocate);  // Строка по правилам упрощенного SQL где вместо имен полей номер поля в фигурных скобках
+
+    bool setFilter(QString sLocate); // Строка по правилам упрощенного SQL
+    bool filterActivate(bool bActive);
+    bool isFilterActive();
+
+    int getFieldCount();
+    int getKeyCount();
+
+    int getRecCount();
+    int getRecNo();
+    bool gotoRecord(int iRecNo);
+
+    bool next();
+    bool previous();
+    bool last();
+    bool first();
+    bool bof();
+    bool eof();
 
 signals:
 
